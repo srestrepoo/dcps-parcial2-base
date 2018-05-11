a449e539299ae9f9d55ef3645802d5aff8a4efc0
srestrepoo <srestrepoo@unal.edu.co>
2018-05-11 12:03:47 -0500

    Eliminar2

--- app/models/author.js
+++ app/models/author.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstname: DS.attr('string'),
-  lastname:DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName:DS.attr('string'),
   username:DS.attr('string'),
   email:DS.attr('string'),
   profilePicture:DS.attr('string'),
